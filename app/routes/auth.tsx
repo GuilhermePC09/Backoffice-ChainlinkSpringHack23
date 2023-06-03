2c6ffd59c85046f75c7ff9f71cf57e35d5e29e12
GuilhermePC09 <guilherme_christo@usp.br>
2023-06-03 17:41:36 -0300

    Fixing Bugs

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,17 +1,12 @@
 import {authentication} from "~/functions/auth";
-import type { ActionArgs, LoaderArgs, V2_MetaFunction} from "@remix-run/node";
-import { Link } from "@remix-run/react";
-
+import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => [{ title: "stableshop • Buy Gift Cards using BTGUSD", description: "stableshop improves the usability of BTG Dol (a form of digital currency) into a payment product aimed at everyday use. Initially, a prepaid card structure called Gift Card will be developed to be used in digital stores. With this product, customers who wish to use BTG Dol for their purchases will be able to use our prepaid card wallet as a gift card. This solution will provide a convenient and affordable way for users to take advantage of BTG Dol in the context of everyday transactions." }];
 
 // Loaders only run on the server and provide data
 // to your component on GET requests
-export const loader = async () : Promise<any> => {
-    return (async function authenticate() {
-        const auth = await authentication();
-        console.log(auth);
-    });
+export const loader = async ({ request }: LoaderArgs) => {
+    return null;
 };
 
 // Actions only run on the server and handle POST
@@ -25,9 +20,11 @@ export async function action({ request }: ActionArgs) {
 // rendered when a route matches the URL. This runs
 // both on the server and the client
 
-export default async function Auth() {
-
-    const loaderResponse = await loader();
+export default function Auth() {
+    async function authenticate() {
+        const auth = await authentication();
+        console.log(auth);
+    }
 
     return (
         <html lang="en">
@@ -36,13 +33,7 @@ export default async function Auth() {
             <meta name="viewport" content="width=device-width,initial-scale=1" />
         </head>
         <body>
-            <button onClick={loaderResponse}>
-                <Link to={`/tracking_page`}>
-                    <span>
-                        Connect Wallet
-                    </span>
-                </Link>
-            </button>
+        <button onClick={authenticate}>Connect Wallet</button>
         </body>
         </html>
     );
