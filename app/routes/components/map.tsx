d99d67ea0c25686232f9d8c50fe38face98b45c4
GuilhermePC09 <guilherme_christo@usp.br>
2023-06-04 02:49:28 -0300

    Route implementation

--- app/routes/components/map.tsx
+++ app/routes/components/map.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Map, Marker, GoogleApiWrapper, IProvidedProps, IMapProps, IMarkerProps } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper, IProvidedProps, IMapProps, IMarkerProps, Polyline } from 'google-maps-react';
 
 interface MapContainerProps extends IProvidedProps {}
 
@@ -7,15 +7,47 @@ class MapContainer extends React.Component<MapContainerProps> {
     render() {
         const mapProps: IMapProps = {
             google: this.props.google!,
-            initialCenter: { lat: -25.344, lng: 131.031 },
+            initialCenter: {lat: -27.467, lng: 153.027},
         };
 
         const markerProps: IMarkerProps = {
-            mapCenter: { lat: -25.344, lng: 131.031 },
+            mapCenter: {lat: -27.467, lng: 153.027},
+        };
+
+
+        const path = [
+            {lat: 37.772, lng: -122.214},
+            {lat: 21.291, lng: -157.821},
+            {lat: -18.142, lng: 178.431},
+            {lat: -27.467, lng: 153.027}
+        ];
+
+        const options = {
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            clickable: false,
+            draggable: false,
+            editable: false,
+            visible: true,
+            radius: 30000,
+            paths: [
+                {lat: 37.772, lng: -122.214},
+                {lat: 21.291, lng: -157.821},
+                {lat: -18.142, lng: 178.431},
+                {lat: -27.467, lng: 153.027}
+            ],
+            zIndex: 1
         };
 
         return (
             <Map {...mapProps}>
+                <Polyline
+                    path={path}
+                    options={options}
+                />
                 <Marker {...markerProps} />
             </Map>
         );
