d0c7fe45d487a8cf9bdddd7a7d4e9a7836900b52
GuilhermePC09 <guilherme_christo@usp.br>
2023-06-04 01:54:45 -0300

    Creating mock DropDown

--- app/routes/tracking_page.tsx
+++ app/routes/tracking_page.tsx
@@ -22,11 +22,19 @@ export default function TrackingPage() {
             <div className="container px-5 py-24 mx-auto flex">
                 <div
                     className="lg:w-1/3 md:w-1/2 bg-gray-900 text-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+
                     <h2 className="text-gray-900 text-white mb-1 font-medium title-font">Wallet</h2>
                     <p className="leading-relaxed mb-5 text-white text-sm">{cookieValue}</p>
+
+
+                    <p className="leading-relaxed mb-1 text-white text-sm">Select one order:</p>
+                    <select className="rounded-t-md rounded-b-md rounded-l-md rounded-r-md border-4 mb-5">
+                        <option value="order1">Order 1</option>
+                    </select>
                     <button
                         onClick={testContract}
-                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-white">Button
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-white">
+                        Register receipt of delivery
                     </button>
                 </div>
             </div>
