a90823123a5b3e51330623f20374a935e6ba0568
GuilhermePC09 <guilherme_christo@usp.br>
2023-06-04 20:36:05 -0300

    Fixing auth bug

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -2,6 +2,8 @@ import {authentication} from "~/functions/auth";
 import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import Cookies from "js-cookie";
+import {useState} from "react";
+import {redirect} from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => [{ title: "stableshop • Buy Gift Cards using BTGUSD", description: "stableshop improves the usability of BTG Dol (a form of digital currency) into a payment product aimed at everyday use. Initially, a prepaid card structure called Gift Card will be developed to be used in digital stores. With this product, customers who wish to use BTG Dol for their purchases will be able to use our prepaid card wallet as a gift card. This solution will provide a convenient and affordable way for users to take advantage of BTG Dol in the context of everyday transactions." }];
 
@@ -24,6 +26,8 @@ export async function action({ request }: ActionArgs) {
 // both on the server and the client
 
 export default function Auth() {
+    const [authenticated, setAuthenticated] = useState(false);
+
     async function authenticate() {
         const hash = await authentication();
 
@@ -33,6 +37,7 @@ export default function Auth() {
         expirationDate.setTime(expirationDate.getTime() + expiresMinutes * 60 * 1000);
 
         Cookies.set("walletHash", hash, { expires: expirationDate });
+        setAuthenticated(true)
     }
 
     return (
@@ -48,14 +53,18 @@ export default function Auth() {
             <section className="flex w-[30rem] flex-col space-y-10">
                 <div className="text-center text-4xl font-medium">Log In</div>
 
-                <button onClick={authenticate}
-                    className="transform rounded-sm bg-indigo-500 py-2 duration-300 hover:bg-indigo-600"
-                >
-                    <Link to={`/tracking_page`}>
-                    <span className="relative text-white">
-                        Connect Wallet
-                    </span>
-                    </Link>
+                <button onClick={authenticate} className="transform rounded-sm bg-indigo-500 py-2 duration-300 hover:bg-indigo-600">
+                    {authenticated ? (
+                        <Link to={`/tracking_page`}>
+                              <span className="relative text-white">
+                                Go to your orders
+                              </span>
+                        </Link>
+                                ) : (
+                                    <span className="relative text-white">
+                                        Connect Wallet
+                                    </span>
+                                )}
                 </button>
             </section>
         </main>
