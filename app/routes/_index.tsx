1dd3b48bb86b2c55a3f8c365aa5a4de0aebe23c0
GuilhermePC09 <guilherme_christo@usp.br>
2023-06-03 22:23:57 -0300

    New Index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
-import * as Auth from "./auth";
+import { redirect } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
@@ -7,20 +7,4 @@ export const meta: V2_MetaFunction = () => {
 
 // Loaders only run on the server and provide data
 // to your component on GET requests
-export const loader = async (args: LoaderArgs) => {
-  return Auth.loader(args);
-};
-
-// Actions only run on the server and handle POST
-// PUT, PATCH, and DELETE. They can also provide data
-// to the component
-export async function action(args: ActionArgs) {
-  return Auth.action(args);
-}
-
-// The default export is the component that will be
-// rendered when a route matches the URL. This runs
-// both on the server and the client
-export default function Index() {
-  return Auth.default();
-}
+export const loader = async () => redirect("/auth");
