53adac23f213d713f8a7bbd67eac623a03698c22
Daniel Piassi <daniel.piassi@outlook.com>
2023-06-11 13:21:58 -0300

    Updated delivery/shipping contract address

--- app/functions/contracts/initialize_blockchain.ts
+++ app/functions/contracts/initialize_blockchain.ts
@@ -1,11 +1,12 @@
 import Web3 from "web3";
 import Contract from "web3-eth-contract";
-import order from "~/contracts/order.json";
+import { AbiItem } from "web3-utils";
 import delivery from "~/contracts/delivery.json";
-import {AbiItem} from "web3-utils";
 import BlockChainConfigDto from "~/functions/dtos/blockChainConfig.dto";
 
-export default async function initializeBlockchain(): Promise<BlockChainConfigDto>{
+const DELIVERY_CONTRACT_ADDRESS = "0x12B50cbD48F814299FcF6782159025427d60E7b8"
+
+export default async function initializeBlockchain(): Promise<BlockChainConfigDto> {
     // 'Web3.givenProvider' will be set if in an Ethereum supported browser.
     const web3Provider = Web3.givenProvider || 'ws://localhost:8546';
 
@@ -15,7 +16,6 @@ export default async function initializeBlockchain(): Promise<BlockChainConfigDt
     // connect to the library contract
 
     const deliveryABI = delivery as AbiItem[];
-    const deliveryAddress = "0xD780fC41baC8F8BC62F5c36a04556c4d79812A10"
 
     // const orderABI = order as AbiItem[];
     // const orderAddress = "0x0f3a3Bc2b56AD73EeD6b574bAE99bfeA83606564"
@@ -24,7 +24,7 @@ export default async function initializeBlockchain(): Promise<BlockChainConfigDt
         web3Provider,
         web3,
         deliveryABI,
-        deliveryAddress,
+        deliveryAddress: DELIVERY_CONTRACT_ADDRESS,
     };
 
 }
